5267077fb00ae7e1b376a147ae64713d825fc7f0
tp-martin <martin.tyler.p@gmail.com>
2025-07-03 20:58:20 -0400

    Create fullName and age variables
    fullName and age variables will combine previous
    ones to optimize
    alter greeting variable to accomodate these new
    additions
    Note spacing and remove the skips from the spec
    file

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -12,8 +12,10 @@ const firstName = "Carlos";
 const lastName = "Stevenson";
 const thisYear = 1965;
 const birthYear = 1947;
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
 
-const greeting = "Hello! My name is" + firstName + lastName + "and I am" + (thisYear - birthYear) + "years old.";
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 console.log (greeting);
 
