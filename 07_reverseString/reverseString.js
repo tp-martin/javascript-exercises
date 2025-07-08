06622c4ae568a8067923d42385e0b6a9543faffd
tp-martin <martin.tyler.p@gmail.com>
2025-07-07 20:44:20 -0400

    Review solution, understand differences
    streamline solution by chaining together the
    discovered methods

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,8 +1,9 @@
 const reverseString = function (str) {
-    const splitString = str.split("");
-    const reverseArray = splitString.reverse();
-    const joinArray = reverseArray.join("");
-    return joinArray;
+    return str.split("").reverse().join("");
+    // const splitString = str.split("");
+    // const reverseArray = splitString.reverse();
+    // const joinArray = reverseArray.join("");
+    // return joinArray;
 };
 
 // Do not edit below this line
