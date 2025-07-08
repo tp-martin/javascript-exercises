48950b2b249eafbd6ff5df8c06c57870b45db7a8
tp-martin <martin.tyler.p@gmail.com>
2025-07-07 20:24:07 -0400

    Pass first test
    create function to split string, reverse, and then
    join back in reversed order

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    const splitString = str.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+    return joinArray;
 };
 
 // Do not edit below this line
