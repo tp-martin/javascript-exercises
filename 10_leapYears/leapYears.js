90bbed8383d5d86d68c32ca2e409dc93e8d78f2e
tp-martin <martin.tyler.p@gmail.com>
2025-07-08 13:41:06 -0400

    Solve leap year problem
    Work through if staments to create conditionals
    get pretty far doing it that way
    get stuck as JS has no 'unless' function
    check solution
    realize making variables then plugging those into
    one if , else statement would be way easier
    keep silly if statements as proof of struggle

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,28 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isCentury = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 === 0;
 
+  if (
+    isYearDivisibleByFour &&
+    (!isCentury || isYearDivisibleByFourHundred)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+
+  
+  
+  
+  // if (year % 4 === 0)
+    //     return true;
+    // if (year % 4 != 0)
+    //     return false;
+    // if (year % 100 === 0)
+    //     return false;
+    // if (year % 100 === 0 && year % 400 === 0)
+    //     return true;
 };
 
 // Do not edit below this line
