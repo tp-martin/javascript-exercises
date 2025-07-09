fef0aa78436fb9a9b7d9104be4af5cbd1bab467f
tp-martin <martin.tyler.p@gmail.com>
2025-07-08 20:30:33 -0400

    Create function for tempConversion
    create a function to convert F to C and vise versa
    make a variable that performs the math
    have return statement ensure answer is a number
    rounded to one decimal place

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = ((fahrenheit - 32) * (5/9))
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * (9/5) + 32)
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
