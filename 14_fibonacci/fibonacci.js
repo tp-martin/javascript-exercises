b958ad0bcfbc1c87be1f70e0f714118440a67874
tp-martin <martin.tyler.p@gmail.com>
2025-07-18 21:00:20 -0400

    fibonacci
    learn what this is
    remember its that golden circle thing from art

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+if (n < 0 ) {
+    return "OOPS";
+} else if (n == 1) {
+    return 1;
+}
+let a = 0;
+let b = 1;
+let result = 0;
 
+for (let i = 2; i <= n; i++) {
+    result = a + b;
+    a = b;
+    b = result;
+}
+return result;
 };
 
 // Do not edit below this line
